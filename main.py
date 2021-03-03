9d8a61d442ecdf735914c7c024fc18856ba7bcac
Louis Mark <spideymans.dev@gmail.com>
2021-03-02 19:53:43 -0500

    detectDiagonalAttacks

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from random import randint
+from collections import namedtuple
 
 class Chromosome:
     def __init__(self):
-        self.queenPositions = [randint(0, 8) for i in range(8)] # Array of 8 random integers
+        self.queenPositions = [randint(0, 3) for i in range(4)] # Array of 8 random integers
+    
+    # Returns the horizontal length of the board
+    # Note: It is assumed that the board is square
+    def boardLength(self):
+        return len(self.queenPositions)
+
+    # Returns the vertical height of the board
+    # Note: It is assumed that the board is square
+    def boardHeight(self):
+        return self.boardLength()
     
     # Makes Chromosome class printable to console
     def __repr__(self):
@@ -12,9 +23,9 @@ class Fitness:
     def __init__(self, chromosome): 
         self.chromosome = chromosome
 
-    # Determines whether any horizontal attacks are possible for any queens on the board.
-    # Returns the number of collisions detected
-    # col: There is always exactly one queen in every column. col is the column number of the queen being checked for collisions.
+    # Determines how many horizontal attacks this individual queen can make. 
+    # Returns the number of possible horizontal attacks.
+    # col: The column number of the queen being evaluated (Note: there is always exactly one queen in every column)
     def detectHorizontalAttacks(self, col):
         masterQueenPosition = self.chromosome.queenPositions[col]
         collisionCount = 0
@@ -23,6 +34,60 @@ class Fitness:
                 collisionCount = collisionCount + 1
         collisionCount = collisionCount - 1 # Subtract 1, becuase the masterQueenPosition was included in collisionCount
         return collisionCount
+    
+    # Determines how many diagonal attacks this individual queen can make. 
+    # Returns the number of possible diagonal attacks.
+    # col: The column number of the queen being evaluated (Note: there is always exactly one queen in every column)
+    def detectDiagonalAttacks(self, col):
+        collisionCount = 0
+
+        # Setup search
+        masterQueenPosition = Position(col, self.chromosome.queenPositions[col])
+
+        # Search diagnoally UP and to the RIGHT
+        searchPosition = Position(masterQueenPosition.x, masterQueenPosition.y) # Reset search position
+        while(searchPosition.x < self.chromosome.boardLength() and searchPosition.y < self.chromosome.boardHeight()):
+            if(self.chromosome.queenPositions[searchPosition.x] == searchPosition.y):
+                collisionCount = collisionCount + 1
+
+            searchPosition.x = searchPosition.x + 1
+            searchPosition.y = searchPosition.y + 1
+        
+        # Search diagnoally DOWN and to the LEFT
+        searchPosition = Position(masterQueenPosition.x, masterQueenPosition.y) # Reset search position
+        while(searchPosition.x > 0 and searchPosition.y > 0):
+            if(self.chromosome.queenPositions[searchPosition.x] == searchPosition.y):
+                collisionCount = collisionCount + 1
+
+            searchPosition.x = searchPosition.x - 1
+            searchPosition.y = searchPosition.y - 1
+
+        # Search diagnoally UP and to the LEFT
+        searchPosition = Position(masterQueenPosition.x, masterQueenPosition.y) # Reset search position
+        while(searchPosition.x > 0 and searchPosition.y < self.chromosome.boardHeight()):
+            if(self.chromosome.queenPositions[searchPosition.x] == searchPosition.y):
+                collisionCount = collisionCount + 1
+
+            searchPosition.x = searchPosition.x - 1
+            searchPosition.y = searchPosition.y + 1
+
+        # Search diagnoally DOWN and to the RIGHT
+        searchPosition = Position(masterQueenPosition.x, masterQueenPosition.y) # Reset search position
+        while(searchPosition.x < self.chromosome.boardLength() and searchPosition.y > 0):
+            if(self.chromosome.queenPositions[searchPosition.x] == searchPosition.y):
+                collisionCount = collisionCount + 1
+
+            searchPosition.x = searchPosition.x + 1
+            searchPosition.y = searchPosition.y - 1
+        
+        print(collisionCount)
+
+
+# Represents x, y positions on the board
+class Position: 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 class Population:
     def __init__(self, size):
@@ -44,4 +109,4 @@ class Runner:
 c = Chromosome()
 print(c)
 f = Fitness(c)
-print(f.detectHorizontalAttacks(0))
+print(f.detectDiagonalAttacks(1))
